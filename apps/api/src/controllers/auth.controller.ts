9920e56491fd15ade11633de05ac0645ef6b5aff
AbhimantraPNW <158009006+AbhimantraPNW@users.noreply.github.com>
2024-05-02 16:00:31 +0700

    Forgot password (#4)
    * chore: initial setup
    
    * feat: add forgot password

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,13 +1,14 @@
+import { forgotPasswordService } from '@/services/auth/forgot-password.service';
 import { loginService } from '@/services/auth/login.service';
 import { registerService } from '@/services/auth/register.service';
 import { NextFunction, Request, Response } from 'express';
 
 export class AuthController {
   async registerController(req: Request, res: Response, next: NextFunction) {
-      try {
-        const result = await registerService(req.body)
-    
-        res.status(200).send(result)
+    try {
+      const result = await registerService(req.body);
+
+      res.status(200).send(result);
     } catch (error) {
       next(error);
     }
@@ -15,11 +16,25 @@ export class AuthController {
 
   async loginController(req: Request, res: Response, next: NextFunction) {
     try {
-      const result = await loginService(req.body)
-  
-      res.status(200).send(result)
-  } catch (error) {
-    next(error);
+      const result = await loginService(req.body);
+
+      res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
   }
-}
+
+  async forgotPasswordController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const result = await forgotPasswordService(req.body.email);
+
+      res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
