ec418c02d29d3f1d03312ad83200b4ee3ac4ce6e
AbhimantraPNW <158009006+AbhimantraPNW@users.noreply.github.com>
2024-05-02 10:40:28 +0700

    Login (#2)
    * chore: initial setup
    
    * feat: add login

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import FormInput from '@/components/FormInput';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { useFormik } from 'formik';
+import { validationSchema } from './validationSchema';
+import useLogin from '@/hooks/api/auth/useLogin';
+
+const Login = () => {
+  const { login } = useLogin();
+  const { values, touched, errors, handleChange, handleBlur, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      validationSchema: validationSchema,
+      onSubmit: async (values) => {
+        login(values);
+      },
+    });
+
+  return (
+    <main className="container mx-auto px-4">
+      <div className="mt-16 flex justify-center">
+        <Card className="w-[350px] ">
+          <CardHeader className="space-y-4">
+            <CardTitle className="text-center text-2xl ">
+              Login
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="grid w-full items-center gap-4">
+                <FormInput
+                  name="email"
+                  error={errors.email}
+                  isError={!!touched.email && !!errors.email}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="example@mail.com"
+                  type="email"
+                  value={values.email}
+                  label="Email"
+                />
+
+                <FormInput
+                  name="password"
+                  error={errors.password}
+                  isError={!!touched.password && !!errors.password}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="password"
+                  type="password"
+                  value={values.password}
+                  label="Password"
+                />
+
+                <Button type="submit" className=" mt-6 w-full text-white">
+                  Login
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-end">
+            {/* <Button className=" text-white">Register</Button> */}
+          </CardFooter>
+        </Card>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
